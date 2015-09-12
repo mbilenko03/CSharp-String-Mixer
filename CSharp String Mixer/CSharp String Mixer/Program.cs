404365142bff463609a139d563bc9482259702d7
Michael B. <mbilenko03@gmail.com>
2015-09-12 13:19:14 -0500

    Finished Randomizer! Added Regions!

--- CSharp String Mixer/CSharp String Mixer/Program.cs
+++ CSharp String Mixer/CSharp String Mixer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace CSharp_String_Mixer
         char[] useTerm = readTerm.ToCharArray();
         Reverser(useTerm);
         CaseSwitch(useTerm); 
+        Randomizer(useTerm);
         }
 
     #region Reverser
@@ -33,11 +35,11 @@ namespace CSharp_String_Mixer
         }
     #endregion
 
-    #region CaseSwitcher
+    #region CaseSwitcher 
     static void CaseSwitch(char[] arrString)
     {
       char[] caseFlip = new char[arrString.Length];
-       char noCase = '?';
+       char noCase = ' ';
        bool result = true;
 
        for(int i = 0; i < arrString.Length; i++)
@@ -63,11 +65,37 @@ namespace CSharp_String_Mixer
        
        if (result == false)
        {
-         Console.WriteLine("There were some input values that could not switch case. They were replaced with ?.");
+         Console.WriteLine("There were some input values that could not switch case. They were replaced with a space.");
        }
+    }
     #endregion
 
+    #region Randomizer
+    static void Randomizer(char[] arrString)
+    {
+      Random r = new Random();
+      char[] RandomArray = new char[arrString.Length];
+      bool[] excludedNumbers = new bool[arrString.Length];
+
+      for (int i = 0; i < arrString.Length; i++)
+      {
+        int arrIndex;
+        do
+        {
+          arrIndex = r.Next(arrString.Length);
+        }
+        while (excludedNumbers[arrIndex] == true);
+
+        RandomArray[i] = arrString[arrIndex];
+        excludedNumbers[arrIndex] = true;
+
+        Debug.WriteLine("Processed index: " + arrIndex);
+      }
+
+      string printRandomizer = new string(RandomArray);
+      Console.WriteLine("Randomizer: " + printRandomizer);
+
     }
- 
+    #endregion
   }
 }
